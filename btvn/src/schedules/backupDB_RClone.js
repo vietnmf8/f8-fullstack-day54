e0b4da6ae0a759a7365e081ed7943015cb687b31
vietnmf8 <vietnmf8@fullstack.edu.vn>
2026-03-04 14:33:08 +0700

    update Rclone

--- btvn/src/schedules/backupDB_RClone.js
+++ btvn/src/schedules/backupDB_RClone.js
@@ -1,61 +1,30 @@
 const dbConfig = require("@/configs/db.config");
-const mailService = require("@/services/mail.service");
 const { getDateStringYmdHis } = require("@/utils/dateFormat");
-const { spawn, execSync } = require("node:child_process");
-const fs = require("fs");
+const { promisify } = require("node:util");
+const { exec: childExec } = require("node:child_process");
 
-async function backupDB_RClone() {
-    /* BackupDB */
+// Biến một hàm trở thành một promise
+const exec = promisify(childExec);
+
+async function backupDB() {
     const { host, user, password, port, database } = dbConfig;
     const dateStr = getDateStringYmdHis();
     const { backupLocalDir, backupRemoteDir, backupRemoteName } = dbConfig;
-    const outputFile = `${backupLocalDir}/${database}_${dateStr}.sql`;
+    const backupCmd = `mysqldump -u${user} -p${password} -h${host} -P${port} ${database} > ${backupLocalDir}/${database}_${dateStr}.sql`;
 
-    // Khởi tạo luồng vào file output
-    const outputStream = fs.createWriteStream(outputFile);
+    try {
+        /* Backup Database về Server */
+        const { stdout, stderr } = await exec(backupCmd);
+        console.log("Backup Success!");
 
-    // Dump file
-    const mysqldump = spawn("mysqldump", [
-        `-u${user}`,
-        `-p${password}`,
-        `-h${host}`,
-        `-P${port}`,
-        `${database}`,
-    ]);
-
-    // Lưu vào file output
-    mysqldump.stdout.pipe(outputStream);
-
-    // Thông báo lỗi
-    mysqldump.on("error", (error) => {
-        outputStream.end();
-        console.log(`Error: ${error.message}`);
-    });
-    // Tiến trình kết thúc
-    mysqldump.on("close", async (code) => {
-        outputStream.end();
-        console.log(`child process exited with code ${code}`);
-
-        // Nếu thành công!
-        if (code === 0) {
-            console.log("Backup thành công!");
-            const copyCmd = `rclone copy ${backupLocalDir} ${backupRemoteName}:${backupRemoteDir}`;
-
-            // Backup lên Google Drive
-            execSync(copyCmd);
-            console.log("Upload Google Drive!");
-
-            /* Gửi email sau khi Backup DB thành công! */
-            await mailService.sendBackupRepost(
-                "vietnmf8@fullstack.edu.vn",
-                `Upload to Google Drive Successfully!`,
-                outputFile,
-            );
-        } else {
-            // Khi tiến trình kết thúc mà lỗi, xoá file đó
-            fs.unlinkSync(outputFile);
-        }
-    });
+        /* Rclone: Copy file Backup lên Google Drive */
+        // rclone copy <thư_mục_nguồn> <tên_remote>:<thư_mục_đích>
+        const copyCmd = `rclone copy ${backupLocalDir} ${backupRemoteName}:${backupRemoteDir}`;
+        await exec(copyCmd);
+        console.log("Upload to Google Drive Successfully!");
+    } catch (error) {
+        console.log(error);
+    }
 }
 
-module.exports = backupDB_RClone;
+module.exports = backupDB;
